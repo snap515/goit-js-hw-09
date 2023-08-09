0ae2b678011f875d650a45fd8f9c8c0ff688cae1
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-08-09 20:51:23 +0200

    installed flatpickr

---
+++ src/js/02-timer.js
@@ -0,0 +1,23 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+const start = document.querySelector('[data-start]');
+
+const flatpickrTimer = flatpickr('#datetime-picker', options);
+
+console.log(flatpickrTimer);
