8fb48ccba5f374a75d9de97b6bf937175c271c48
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-08-09 19:59:12 +0200

    1 task completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+function onColorChange() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener('click', onStartBtnPress);
+stopBtn.addEventListener('click', onStopBtnPress);
+
+function onStartBtnPress() {
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+
+  onColorChange();
+
+  timerId = setInterval(onColorChange, 1000);
+}
+
+function onStopBtnPress() {
+  stopBtn.setAttribute('disabled', '');
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+}
