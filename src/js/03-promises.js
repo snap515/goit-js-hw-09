a18afed1d14c7ab45490337b79f888f2df9262c9
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-08-23 20:22:31 +0200

    update task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,22 +11,23 @@ let amount;
 
 formEl.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(e) {
+  e.preventDefault();
+  getFormData();
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(result => Notiflix.Notify.success(result))
+      .catch(error => Notiflix.Notify.failure(error));
+    delay += step;
+  }
+}
+
 function getFormData() {
   delay = delayEl.valueAsNumber;
   step = stepEl.valueAsNumber;
   amount = amountEl.valueAsNumber;
 }
 
-function onFormSubmit(e) {
-  e.preventDefault();
-  getFormData();
-  for (let i = 1; i <= amount; i += 1) {
-    createPromise(i, delay)
-      .then(result => Notiflix.Notify.success(result))
-      .catch(error => Notiflix.Notify.failure(error));
-    delay += step;
-  }
-}
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -35,11 +36,7 @@ function createPromise(position, delay) {
         resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
       } else {
         reject(`❌ Rejected promise ${position} in ${delay}ms`);
-        // Reject
       }
     }, delay);
   });
 }
-
-// Notiflix.Notify.success('SUCCESS');
-// Notiflix.Notify.failure('FAIL');
