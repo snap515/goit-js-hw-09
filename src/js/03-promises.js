2363af0a9ffd34e0577db0ca9b833c525e49adc4
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-08-23 20:15:38 +0200

    finished 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('[name=delay]');
+const stepEl = document.querySelector('[name=step]');
+const amountEl = document.querySelector('[name=amount]');
+
+let delay;
+let step;
+let amount;
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function getFormData() {
+  delay = delayEl.valueAsNumber;
+  step = stepEl.valueAsNumber;
+  amount = amountEl.valueAsNumber;
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  getFormData();
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(result => Notiflix.Notify.success(result))
+      .catch(error => Notiflix.Notify.failure(error));
+    delay += step;
   }
 }
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        // Reject
+      }
+    }, delay);
+  });
+}
+
+// Notiflix.Notify.success('SUCCESS');
+// Notiflix.Notify.failure('FAIL');
